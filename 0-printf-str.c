af96f294df4eeecb94321a6518d3c66976c61fe4
Reem <ireosama1@gmail.com>
2023-09-17 11:49:01 +0400

    processing %s %% %c %d %i

---
+++ 0-printf-str.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _printfstring - handles the cases of %s specifier
+ * @args: the argument used
+ *
+ * Return: length of the string
+ */
+
+int _printfstring(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	fputs(s, stdout);
+	return (strlen(s));
+}
+
+/**
+ * _printfchar - handles the cases of the %, and %c sepecifiers.
+ * @args: The argument used
+ *
+ * Return: 1
+ */
+
+int _printfchar(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	putchar(c);
+	return (1);
+}
