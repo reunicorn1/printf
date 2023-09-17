db3aadf89ff33f3bea7580f17581e588fef28e3c
Reem <ireosama1@gmail.com>
2023-09-17 23:37:06 +0400

    edits on _printf.c

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(arg, format);
-	if (format == NULL || format[0] == '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	for (i = 0, sum = 0; format[i]; i++)
 	{
