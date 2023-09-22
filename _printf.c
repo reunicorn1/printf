e3f5145a4cd9905108aa942bd9431a719425dc93
MD2305 <drissmellouki96@gmail.com>
2023-09-22 12:41:19 -0700

    3edit

--- _printf.c
+++ _printf.c
@@ -3,9 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+
 /**
  * _printf - takes a formatted string as input to produce output.
  * @format: formatted string, may or may not have placeholders.
  *
  * Return: returns the number of characters printed.
  */
+
+int _printf(const char *format, ...)
+{
